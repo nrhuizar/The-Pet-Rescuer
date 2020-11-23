7db74d607d1f7c678395b27f102456509a07ba05
nrhuizar <nrhuizar@gmail.com>
2020-11-23 15:06:44 -0800

    added pet details modal and search results display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,53 @@
+var petContainerE1 = document.querySelector("#pet-container");
+var petModal = document.querySelector("#petModal");
+var btnClose = document.querySelector("#btnClose");
+var span = document.getElementsByClassName("close")[0];
+
+// Display Pet Results
+var displayPets = function(animals) {
+    if (animals.length === 0) {
+        petContainerE1.textContent = "No Pets Found.";
+        return;
+    }
+
+    petContainerE1.textContent = "";
+
+    for (var i = 0; i < animals.length; i++) {
+        var petName = animals[i].name;
+
+        var petE1 = document.createElement("a");
+        petE1.classList = "content";
+        
+        petName = document.createElement("span");
+        petName.textContent = petName;
+
+        petE1.appendChild(petE1);
+    }
+};
+
+// Opens Pet Modal
+petContainerE1.onclick = function() {
+    petModal.classList = "is-active";
+};
+
+
+
+// Closes Modal
+btnClose.onclick = function() {
+    petModal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == petModal) {
+        petModal.style.display = "none";       
+    }
+};
+
+
+// Hero Carousel
 bulmaCarousel.attach('#carousel-demo', {
     slidesToScroll: 1,
     slidesToShow: 4
 });
+
+petContainerE1.addEventListener("click", petModal);
